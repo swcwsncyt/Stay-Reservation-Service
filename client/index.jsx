a55b91fd36bb552b82b64ae5de3795767dc8034b
swcwsncyt <swcwsncyt@gmail.com>
2019-08-29 12:51:34 -0700

    files add after eslint fix

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 import RoomDetails from './components/RoomDetails.jsx';
 import Date from './components/Date.jsx';
 import Guest from './components/Guest.jsx';
 import Total from './components/Total.jsx';
 import Reserve from './components/ReserveBtn.jsx';
-import styled from 'styled-components';
 
 const ResModule = styled.div`
   color: rgb(72, 72, 72);
@@ -15,23 +15,23 @@ const ResModule = styled.div`
   padding-bottom: 16px;
   border: 1px rgb(228, 228, 228) solid;
   border-radius: 4px;
-`
+`;
 const ResInner = styled.div`
   width: 326px;
   height: 466px;
   margin: auto;
-`
+`;
 const ResLine = styled.div`
   width: 326px;
   height: 1px;
   background-color: rgb(235, 235, 235);
   margin-top: 16px;
   margin-bottom: 16px;
-`
+`;
 const ResForm = styled.form`
   width: 326px;
   height: 385px;
-`
+`;
 const ResChargeStatement = styled.div`
   width: 326px;
   height: 20px;
@@ -39,7 +39,7 @@ const ResChargeStatement = styled.div`
   text-align: center;
   font-size: 12px;
   font-weight: 600;
-`
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -47,83 +47,88 @@ class App extends React.Component {
 
     this.state = {
       resStyle: {
-        float: "right",
-        marginRight: "150px",
-        marginTop: "52px",
+        float: 'right',
+        marginRight: '150px',
+        marginTop: '52px',
         listing: {},
-        booking: []
-      }
-    }
+        booking: [],
+      },
+    };
     this.initialData = this.getInitialData.bind(this);
   }
+
   componentDidMount() {
     this.initialData();
     window.addEventListener('scroll', this.onScroll.bind(this));
   }
+
   getInitialData() {
-    axios.post('/api/reservation/search', {id: 50})
-    .then((res) => {
-      this.setState({
-        listing: res.data.listing[0],
-        booking: res.data.booking
+    axios.post('/api/reservation/search', { id: 50 })
+      .then((res) => {
+        this.setState({
+          listing: res.data.listing[0],
+          booking: res.data.booking,
+        });
       })
-    })
-    .catch((err) => {
-      console.log("err")
-    })
+      .catch((err) => {
+        console.log('err');
+      });
   }
+
   onScroll(e) {
     if (e.target.scrollingElement.scrollTop > 527 && e.target.scrollingElement.scrollTop < 1688) {
       this.setState({
         resStyle: {
-          right: "158px",
-          position: "fixed",
-          top: "0px",
-          marginTop: "52px"
-        }
-      })
+          right: '158px',
+          position: 'fixed',
+          top: '0px',
+          marginTop: '52px',
+        },
+      });
     }
     if (e.target.scrollingElement.scrollTop < 527) {
       this.setState({
         resStyle: {
-          float: "right",
-          marginRight: "150px",
-          marginTop: "52px"
-        }
-      })
+          float: 'right',
+          marginRight: '150px',
+          marginTop: '52px',
+        },
+      });
     }
     if (e.target.scrollingElement.scrollTop > 1688) {
       this.setState({
         resStyle: {
-          float: "right",
-          marginRight: "150px",
-          marginTop: "1220px"
-        }
-      })
+          float: 'right',
+          marginRight: '150px',
+          marginTop: '1220px',
+        },
+      });
     }
   }
+
   onSubmit(e) {
     e.preventDefault();
   }
+
   render() {
     return (
-    <ResModule style={this.state.resStyle}>
-        <ResInner >
-          <RoomDetails details={this.state.listing}/>
+      <ResModule style={this.state.resStyle}>
+        <ResInner>
+          <RoomDetails details={this.state.listing} />
           <ResLine />
           <ResForm onSubmit={this.onSubmit.bind(this)}>
-            <Date booking={this.state.booking}/>
-            <Guest details={this.state.listing}/>
-            <Total details={this.state.listing}/>
+            <Date booking={this.state.booking} />
+            <Guest details={this.state.listing} />
+            <Total details={this.state.listing} />
             <Reserve />
             <ResChargeStatement>You won’t be charged yet</ResChargeStatement>
           </ResForm>
         </ResInner>
-    </ResModule>
-    )
+      </ResModule>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("res"))
-//style={this.state.resStyle}
+ReactDOM.render(<App />, document.getElementById('res'));
+// style={this.state.resStyle}
 export default App;
